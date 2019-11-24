eadab92dd2122dfb2e9519d846b26c100bb1b143
Baktiar Miah <baktiar.m@gmail.com>
2019-11-24 22:33:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,13 @@ class App < Sinatra::Base
 
   get '/name' do
     'Baktiar Miah '
-  end 
+  end
 
+ get '/hometown' do
+   'Brooklyn, NY'
+ end
 
+ get '/favorite-song' do
+   'Numb - Linkin Park '
+ end 
 end
